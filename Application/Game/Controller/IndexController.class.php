8496cece96a3e42b6835bff95db53a2f363284d7
jeakey <june_fh@qq.com>
2014-10-14 22:57:09 +0800

    新增结束页面

--- Application/Game/Controller/IndexController.class.php
+++ Application/Game/Controller/IndexController.class.php
@@ -4,18 +4,19 @@ use Think\Controller;
 use \LaneWeChat\Userinfo;
 class IndexController extends Controller {
     public function index(){
+        // echo strtotime("2014-10-14 23:00:00");
+        $overtime1=strtotime("2014-10-16 00:00:00");
+        $overtime2=strtotime("2014-10-21 00:00:00");
 
-		// $Info=new \LaneWeChat\Userinfo();
-		// $userinfo=$Info->getUserinfo();
-		// $urlparam=$Info->getUrlparam();
-		// $this->assign('userinfo',$userinfo);
-		// $this->assign('urlparam',$urlparam);
-        // $type=$_GET["type"];
-        // $this->display();
-        // echo $_GET("simple");
-        if(isset($_GET["simple"])){
-            $this->display("index2");
-        }else{
+
+        // echo (strtotime("2014-10-14 23:00:00")-time());
+
+        if($overtime2-time()<0){
+            $this->display("end2");
+        }else if($overtime1-time()<0){
+             $this->display("end");
+        }
+        else{
             $this->display();
         }
         
